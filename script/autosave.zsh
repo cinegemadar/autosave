edab6847b850dcd2d4d28ed436b6a688356edd88
David Balogh <balogh.david@gmail.com>
2020-04-18 15:03:50 +0200

    2020-04-18 15:03:50.212989 autosave

--- script/autosave.zsh
+++ script/autosave.zsh
@@ -1,3 +1,7 @@
 #!/bin/zsh
-cd /Users/dbalogh/dev/python/autosave
-/Users/dbalogh/dev/python/envs/autosave/bin/python autosave.py
+# crete similar script and add it to corntab (linux/osx)
+# 1.) chmod +x /pathtoyourscript/autosave.zsh
+# 2.) corntab -e
+# 3.) * * * * * /pathtoyourscript/autosave.zsh
+cd /Users/dbalogh/dev/python/autosave # containing folder of autosave.py
+/Users/dbalogh/dev/python/envs/autosave/bin/python autosave.py #path to your python3 executable
