d0a950c20e4d6ff10f79f9ba423044b510d811f7
David Balogh <balogh.david@gmail.com>
2020-04-18 13:53:54 +0200

    {datetime.timestamp.now()} autosave

--- autosave.py
+++ autosave.py
@@ -1,10 +1,12 @@
 import git
 from os import linesep 
+from datetime import datetime
 
 cmd = git.cmd.Git()
 res = cmd.status(porcelain=True)
 filesToBeSaved = len(res.split(linesep)) > 0
 if filesToBeSaved:
     cmd.add(".")
-    cmd.commit(message="autosave")
+    now = datetime.now()
+    cmd.commit(message="{datetime.timestamp.now()} autosave")
     cmd.push()
