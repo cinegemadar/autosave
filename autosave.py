050e91e677168f54863d2c7d5a48d7034c5c2d49
David Balogh <balogh.david@gmail.com>
2020-04-18 14:11:18 +0200

    2020-04-18 14:11:18.230691 autosave

--- autosave.py
+++ autosave.py
@@ -3,7 +3,7 @@ from os import linesep
 from datetime import datetime
 
 cmd = git.cmd.Git()
-if "" != cmd.status(porcelain=True):
+if cmd.status(porcelain=True):
     cmd.add(".")
     cmd.commit(message=f"{datetime.now()} autosave")
     cmd.push()
