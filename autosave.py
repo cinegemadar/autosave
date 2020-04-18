c9d24ebc6ce6ae6849262860c95580bc41622ab1
David Balogh <balogh.david@gmail.com>
2020-04-18 14:19:59 +0200

    2020-04-18 14:19:59.009964 autosave

--- autosave.py
+++ autosave.py
@@ -2,8 +2,17 @@ import git
 from os import linesep 
 from datetime import datetime
 
-cmd = git.cmd.Git()
-if cmd.status(porcelain=True):
-    cmd.add(".")
-    cmd.commit(message=f"{datetime.now()} autosave")
-    cmd.push()
+        
+
+def autosave():
+    cmd = git.cmd.Git()
+    if cmd.status(porcelain=True):
+        cmd.add(".")
+        cmd.commit(message=f"{datetime.now()} autosave")
+        cmd.push()
+        
+if __name__ == "__main__":
+    try:
+        autosave()
+    except:
+        print("fatal: not a git repository (or any of the parent directories)")
