89951312bfab011517872dd0506d30055be247de
David Balogh <balogh.david@gmail.com>
2020-04-18 14:27:41 +0200

    2020-04-18 14:27:41.970160 autosave

--- autosave.py
+++ autosave.py
@@ -1,18 +1,21 @@
 import git
-from os import linesep 
+from os import linesep
 from datetime import datetime
+import json
 
-        
-
-def autosave():
-    cmd = git.cmd.Git()
+def autosave(path):
+    cmd = git.cmd.Git(path)
     if cmd.status(porcelain=True):
         cmd.add(".")
         cmd.commit(message=f"{datetime.now()} autosave")
         cmd.push()
-        
+
+
 if __name__ == "__main__":
-    try:
-        autosave()
-    except:
-        print("fatal: not a git repository (or any of the parent directories)")
+    with open("autosave.json") as jsonFile:
+        data = json.load(jsonFile)
+    # try:
+    for path in data["folders"]:
+        autosave(path)
+    # except:
+    #     print("fatal: not a git repository (or any of the parent directories)")
