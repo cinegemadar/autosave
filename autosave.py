0339bf041b82830b64c9571f5fcf79408df60bc1
David Balogh <balogh.david@gmail.com>
2020-04-18 14:07:55 +0200

    test

--- autosave.py
+++ autosave.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 cmd = git.cmd.Git()
 res = cmd.status(porcelain=True)
-filesToBeSaved = len(res.split(linesep)) > 0
+filesToBeSaved = "" != res and len(res.split(linesep)) > 0
 if filesToBeSaved:
     print(len(res.split(linesep)))
     print(res)
